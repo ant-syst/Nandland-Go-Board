c3217c39ba0a9b17d2f77beb9425670bddb6902b
antoine <antoine>
2020-02-02 19:01:23 +0100

    3_flip_flop.vhd: update comments and labels name

--- src/3_flip_flop.vhd
+++ src/3_flip_flop.vhd
@@ -1,21 +1,23 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity And_Gate is
+entity Clocked_Logic is
     port (
         i_Clk      : in std_logic;
         i_Switch_1 : in std_logic;
         o_LED_1    : out std_logic
     );
-end entity And_Gate;
+end entity Clocked_Logic;
 
-architecture RTL of And_Gate is
+architecture RTL of Clocked_Logic is
 
     signal r_Switch_1 : std_logic := '0';
     signal r_LED_1    : std_logic := '0';
 
 begin
-
+    -- sequential process
+    -- When the i_Clk changes (goes from 0 to 1 or from 1 to 0)
+    -- the process is executed
     p_Register : process (i_Clk) is
     begin
         if rising_edge(i_Clk) then
@@ -31,8 +33,21 @@ begin
         end if;
     end process;
     
-    
     -- on the next call 
     o_LED_1 <= r_LED_1;
 end
 architecture RTL;
+
+-- process 
+-- is 
+-- triggered    v     v     v     v
+--               __    __    __    __
+-- i_Clk      __|  |__|  |__|  |__|  |__
+--                _____
+-- i_Switch_1 ___|     |________________
+--                     _____
+-- r_Switch_1 ________|     |___________
+--                           ___________
+-- r_LED_1    ______________|
+--                                 _____
+-- o_LED_1    ____________________|
