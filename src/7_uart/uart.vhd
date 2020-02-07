98cbc5b5694a1eeeb4ab2e411c6cb25f7dd7ba7c
antoine <antoine>
2020-02-07 17:09:45 +0100

    Clean useless variables

--- src/7_uart/uart.vhd
+++ src/7_uart/uart.vhd
@@ -9,11 +9,6 @@ entity UART is
 
         i_UART_RX   : in std_logic;
 
-        o_LED_1      : out std_logic;
-        o_LED_2      : out std_logic;
-        o_LED_3      : out std_logic;
-        o_LED_4      : out std_logic;
-
         o_Segment1_A : out std_logic;
         o_Segment1_B : out std_logic;
         o_Segment1_C : out std_logic;
@@ -42,18 +37,12 @@ architecture RTL of UART is
 
     signal right_digit : integer := 0;
     signal left_digit : integer := 0;
-    signal r_LedCounter : integer range 0 to 4 := 0;
     signal r_TimeCount : integer range 0 to 10000 := 0;
     signal r_time      : integer range 0 to 1000 := 0;
                                                  -- 250000 10 ms
-    signal r_LED_1     : std_logic := '0';
-    signal r_LED_2     : std_logic := '0';
-    signal r_LED_3     : std_logic := '0';
-    signal r_LED_4     : std_logic := '0';
 
     signal r_counter   : integer range 0 to 10 := 0;
     signal r_num2      : std_logic_vector(7 downto 0) := "00000000";
-    signal r_num3      : std_logic_vector(3 downto 0) := "0000";
     signal r_state     : T_STATE := STOPPED;
 
     signal w_Segment1_A : std_logic := '0';
@@ -81,25 +70,8 @@ begin
             then
                 if i_UART_RX = '0'
                 then
-                    r_LED_1 <= '1';
                     r_time <= 217 / 2;
                     r_state <= STARTING;
-
-                    --w_Segment2_A  <= '0';
-                    --w_Segment2_B  <= '0';
-                    --w_Segment2_C  <= '0';
-                    --w_Segment2_D  <= '0';
-                    --w_Segment2_E  <= '0';
-                    --w_Segment2_F  <= '0';
-                    --w_Segment2_G  <= '0';
-
-                    --w_Segment1_A  <= '0';
-                    --w_Segment1_B  <= '0';
-                    --w_Segment1_C  <= '0';
-                    --w_Segment1_D  <= '0';
-                    --w_Segment1_E  <= '0';
-                    --w_Segment1_F  <= '0';
-                    --w_Segment1_G  <= '0';
 
                     r_num2(0) <= '0';
                     r_num2(1) <= '0';
@@ -109,11 +81,6 @@ begin
                     r_num2(5) <= '0';
                     r_num2(6) <= '0';
                     r_num2(7) <= '0';
-
-                    r_LED_2 <= '0';
-                    r_LED_3 <= '0';
-                    r_LED_4 <= '0';
-                    --r_time <= 0;
                     r_counter <= 0;
                 end if;
 
@@ -125,11 +92,9 @@ begin
                 else
                     if i_UART_RX = '0'
                     then
-                        r_LED_2 <= '1';
                         r_state <= STARTED;
                         r_time <= 217;
                     else
-                        r_LED_4 <= '1';
                         r_state <= FAILED;
                     end if;
 
@@ -160,57 +125,7 @@ begin
                         then
                             r_num2(r_counter) <= '1';
                             r_counter <= r_counter + 1;
-
-                            --if r_counter = 0
-                            --then
-                            --    w_Segment2_A  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 1
-                            --then
-                            --    w_Segment2_B  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 2
-                            --then
-                            --    w_Segment2_C  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 3
-                            --then
-                            --    w_Segment2_D  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 4
-                            --then
-                            --    w_Segment2_E  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 5
-                            --then
-                            --    w_Segment2_F  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 6
-                            --then
-                            --    w_Segment2_G  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 7
-                            --then
-                                --r_LED_3 <= '1';
-                            --    w_Segment1_A  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 8
-                            --then
-                            --    w_Segment1_B  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --elsif r_counter = 9
-                            --then
-                            --    w_Segment1_C  <= '1';
-                            --    r_counter <= r_counter + 1;
-                            --else
-                            --    w_Segment1_D  <= '1';
-                            --    --r_counter <= r_counter + 1;
-                            --end if;
-
-                        --r_LED_2 <= '0';
                         else
-                            --r_LED_1 <= '0';
-                            --r_LED_2 <= '1';
                             r_num2(r_counter) <= '0';
                             r_counter <= r_counter + 1;
                         end if;
@@ -220,29 +135,6 @@ begin
 
         end if;
     end process;
-
-    o_LED_1 <= r_LED_1;
-    o_LED_2 <= r_LED_2;
-    o_LED_3 <= r_LED_3;
-    o_LED_4 <= r_LED_4;
-
-    --w_Segment2_A  <= r_num2(0);
-    --w_Segment2_B  <= r_num2(1);
-    --w_Segment2_C  <= r_num2(2);
-    --w_Segment2_D  <= r_num2(3);
-    --w_Segment2_E  <= r_num2(4);
-    --w_Segment2_F  <= r_num2(5);
-    --w_Segment2_G  <= r_num2(6);
-
-    --w_Segment1_A  <= r_num2(7);
-    --w_Segment1_B  <= '0';
-    --w_Segment1_C  <= '0';
-    --w_Segment1_D  <= '0';
-    --w_Segment1_E  <= '0';
-    --w_Segment1_F  <= '0';
-    --w_Segment1_G  <= '0';
-
-    --o_LED_3 <= not w_Segment1_A;
 
     left_digit <= to_integer(unsigned(r_num2)) / 16;
     right_digit <= to_integer(unsigned(r_num2)) mod 16;
@@ -272,8 +164,6 @@ begin
         o_Segment_F  => w_Segment1_F,
         o_Segment_G  => w_Segment1_G
     );
-
-    --w_Segment2_A  <= '0';
 
     o_Segment2_A  <= not w_Segment2_A;
     o_Segment2_B  <= not w_Segment2_B;
