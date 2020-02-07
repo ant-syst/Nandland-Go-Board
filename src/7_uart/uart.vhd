4a19b97ded98cb67cf8bf150ace3492f5fe06ee9
antoine <antoine>
2020-02-07 17:29:05 +0100

    CLeaning and renaming

--- src/7_uart/uart.vhd
+++ src/7_uart/uart.vhd
@@ -38,11 +38,8 @@ architecture RTL of UART is
     signal right_digit : integer := 0;
     signal left_digit : integer := 0;
     signal r_TimeCount : integer range 0 to 10000 := 0;
-    signal r_time      : integer range 0 to 1000 := 0;
-                                                 -- 250000 10 ms
-
     signal r_counter   : integer range 0 to 10 := 0;
-    signal r_num2      : std_logic_vector(7 downto 0) := "00000000";
+    signal r_bits      : std_logic_vector(7 downto 0) := "00000000";
     signal r_state     : T_STATE := STOPPED;
 
     signal w_Segment1_A : std_logic := '0';
@@ -70,47 +67,45 @@ begin
             then
                 if i_UART_RX = '0'
                 then
-                    r_time <= 217 / 2;
                     r_state <= STARTING;
 
-                    r_num2(0) <= '0';
-                    r_num2(1) <= '0';
-                    r_num2(2) <= '0';
-                    r_num2(3) <= '0';
-                    r_num2(4) <= '0';
-                    r_num2(5) <= '0';
-                    r_num2(6) <= '0';
-                    r_num2(7) <= '0';
+                    r_bits(0) <= '0';
+                    r_bits(1) <= '0';
+                    r_bits(2) <= '0';
+                    r_bits(3) <= '0';
+                    r_bits(4) <= '0';
+                    r_bits(5) <= '0';
+                    r_bits(6) <= '0';
+                    r_bits(7) <= '0';
                     r_counter <= 0;
+                    r_TimeCount <= 0;
                 end if;
 
             elsif r_state = STARTING
             then
-                if r_TimeCount < r_time
+                if r_TimeCount < (217 / 2)
                 then
                     r_TimeCount <= r_TimeCount + 1;
                 else
+                    r_TimeCount <= 0;
+
                     if i_UART_RX = '0'
                     then
                         r_state <= STARTED;
-                        r_time <= 217;
                     else
                         r_state <= FAILED;
                     end if;
-
-                    r_TimeCount <= 0;
                 end if;
             elsif r_state = FAILED
             then
                 r_state <= STOPPED;
             elsif r_state = STARTED
             then
-                if r_TimeCount < r_time
+                if r_TimeCount < 217
                 then
                     r_TimeCount <= r_TimeCount + 1;
                 else
                     r_TimeCount <= 0;
-                    r_time <= 217;
 
                     if r_counter = 8
                     then
@@ -123,21 +118,20 @@ begin
                     else
                         if i_UART_RX = '1'
                         then
-                            r_num2(r_counter) <= '1';
+                            r_bits(r_counter) <= '1';
                             r_counter <= r_counter + 1;
                         else
-                            r_num2(r_counter) <= '0';
+                            r_bits(r_counter) <= '0';
                             r_counter <= r_counter + 1;
                         end if;
                     end if;
                 end if;
             end if;
-
         end if;
     end process;
 
-    left_digit <= to_integer(unsigned(r_num2)) / 16;
-    right_digit <= to_integer(unsigned(r_num2)) mod 16;
+    left_digit <= to_integer(unsigned(r_bits)) / 16;
+    right_digit <= to_integer(unsigned(r_bits)) mod 16;
 
     SevenSeg1_Inst : entity work.Binary_To_7Segment
     port map (
