56bceacfad0ea1fdd49e6aebb66431942c2ee5f9
antoine <antoine>
2020-02-07 22:55:28 +0100

    Add assertion in test

--- src/7_uart/testbench.vhd
+++ src/7_uart/testbench.vhd
@@ -10,6 +10,7 @@ architecture behave of UART_TB is
     constant c_CLK_PERIOD : time := 1 ns;
     signal r_Clock : std_logic   := '0';
     signal r_UART : std_logic    := '1';
+    signal r_Byte : std_logic_vector(7 downto 0);
 
 begin
 
@@ -24,13 +25,21 @@ begin
         i_Clk         => r_Clock,
         i_UART_RX     => r_UART,
         o_Has_Failed  => open,
-        o_Byte        => open
+        o_Byte        => r_Byte
     );
 
     process is
     begin
         report "Starting Testbench...";
+
         wait for 0.11 us;
+
+        if r_Byte = X"55" then
+            report "Test Passed - Correct Byte Received" severity note;
+        else
+            report "Test Failed - Incorrect Byte Received" severity note;
+        end if;
+
         assert false report "Test Complete" severity failure;
     end process;
 
