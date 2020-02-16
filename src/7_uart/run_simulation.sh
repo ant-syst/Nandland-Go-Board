36b136a958dd406ca2d70d13069192ef840543e5
antoine <antoine>
2020-02-16 12:10:27 +0100

    Renaming testbench and update run_simulation script to start a gtkwave program

--- src/7_uart/run_simulation.sh
+++ src/7_uart/run_simulation.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+set -u
+
+if [[ "$#" == 1 ]]
+then
+    if [[ "$1" == "--gtkwave" ]]
+    then
+        run_gtkwave=1
+    else
+        echo "Usage $0 [--gtkwave]"
+        exit 1
+    fi
+else
+    run_gtkwave=0
+fi
+
 cd "$(dirname "$(readlink -f "$0")")"
 
 rm -f work-obj93.cf
 
 ${GHDL_BIN} -a Binary_To_7Segment.vhd
 ${GHDL_BIN} -a uart_receiver.vhd
-${GHDL_BIN} -a testbench.vhd
+${GHDL_BIN} -a test_bench.vhd
 ${GHDL_BIN} -e UART_TB
-${GHDL_BIN} -r UART_TB --vcd=testbench.vcd
+${GHDL_BIN} -r UART_TB --vcd=test_bench.vcd
 
-gtkwave testbench.vcd &
+if [[ "${run_gtkwave}" == 1 ]]
+then
+    gtkwave testbench.vcd &
+fi
