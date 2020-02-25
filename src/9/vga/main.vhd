c11ba1f92c34b8c99adfa6cd165bae3f791bf27b
antoine <antoine>
2020-02-25 12:00:50 +0100

    Add 7 segment display

--- src/9/vga/main.vhd
+++ src/9/vga/main.vhd
@@ -32,7 +32,22 @@ entity VGA is
         o_VGA_Grn_2 : out std_logic;
         o_VGA_Blu_0 : out std_logic;
         o_VGA_Blu_1 : out std_logic;
-        o_VGA_Blu_2 : out std_logic
+        o_VGA_Blu_2 : out std_logic;
+
+        o_Segment1_A : out std_logic;
+        o_Segment1_B : out std_logic;
+        o_Segment1_C : out std_logic;
+        o_Segment1_D : out std_logic;
+        o_Segment1_E : out std_logic;
+        o_Segment1_F : out std_logic;
+        o_Segment1_G : out std_logic;
+        o_Segment2_A : out std_logic;
+        o_Segment2_B : out std_logic;
+        o_Segment2_C : out std_logic;
+        o_Segment2_D : out std_logic;
+        o_Segment2_E : out std_logic;
+        o_Segment2_F : out std_logic;
+        o_Segment2_G : out std_logic
     );
 end entity VGA;
 
@@ -79,6 +94,22 @@ architecture RTL of VGA is
     signal r_Bits_DV    : std_logic := '0';
     signal r_Has_Failed : std_logic := '0';
 
+    signal w_Segment1_A : std_logic := '0';
+    signal w_Segment1_B : std_logic := '0';
+    signal w_Segment1_C : std_logic := '0';
+    signal w_Segment1_D : std_logic := '0';
+    signal w_Segment1_E : std_logic := '0';
+    signal w_Segment1_F : std_logic := '0';
+    signal w_Segment1_G : std_logic := '0';
+
+    signal w_Segment2_A : std_logic := '0';
+    signal w_Segment2_B : std_logic := '0';
+    signal w_Segment2_C : std_logic := '0';
+    signal w_Segment2_D : std_logic := '0';
+    signal w_Segment2_E : std_logic := '0';
+    signal w_Segment2_F : std_logic := '0';
+    signal w_Segment2_G : std_logic := '0';
+
     signal r_VGA_HSync : std_logic := '0';
     signal r_VGA_VSync : std_logic := '0';
 
@@ -109,6 +140,50 @@ begin
         i_Clk     => i_Clk,
         o_UART_TX => r_UART_TX
     );
+
+    o_UART_TX   <= r_UART_TX;
+
+    SevenSeg1_Inst : entity work.Binary_To_7Segment
+    port map (
+        i_Clk        => i_Clk,
+        i_Binary_Num => r_Bits(3 downto 0),
+        o_Segment_A  => w_Segment2_A,
+        o_Segment_B  => w_Segment2_B,
+        o_Segment_C  => w_Segment2_C,
+        o_Segment_D  => w_Segment2_D,
+        o_Segment_E  => w_Segment2_E,
+        o_Segment_F  => w_Segment2_F,
+        o_Segment_G  => w_Segment2_G
+    );
+
+    o_Segment2_A  <= not w_Segment2_A;
+    o_Segment2_B  <= not w_Segment2_B;
+    o_Segment2_C  <= not w_Segment2_C;
+    o_Segment2_D  <= not w_Segment2_D;
+    o_Segment2_E  <= not w_Segment2_E;
+    o_Segment2_F  <= not w_Segment2_F;
+    o_Segment2_G  <= not w_Segment2_G;
+
+    SevenSeg2_Inst : entity work.Binary_To_7Segment
+    port map (
+        i_Clk        => i_Clk,
+        i_Binary_Num => r_Bits(7 downto 4),
+        o_Segment_A  => w_Segment1_A,
+        o_Segment_B  => w_Segment1_B,
+        o_Segment_C  => w_Segment1_C,
+        o_Segment_D  => w_Segment1_D,
+        o_Segment_E  => w_Segment1_E,
+        o_Segment_F  => w_Segment1_F,
+        o_Segment_G  => w_Segment1_G
+    );
+
+    o_Segment1_A  <= not w_Segment1_A;
+    o_Segment1_B  <= not w_Segment1_B;
+    o_Segment1_C  <= not w_Segment1_C;
+    o_Segment1_D  <= not w_Segment1_D;
+    o_Segment1_E  <= not w_Segment1_E;
+    o_Segment1_F  <= not w_Segment1_F;
+    o_Segment1_G  <= not w_Segment1_G;
 
     VGA_Sync_Pulses_Inst : entity work.VGA_Sync_Pulses
     generic map (
@@ -172,7 +247,6 @@ begin
         o_VGA_VSync => r_VGA_VSync2
     );
 
-    o_UART_TX   <= r_UART_TX;
     o_VGA_VSync <= r_VGA_VSync2;
     o_VGA_HSync <= r_VGA_HSync2;
 end
